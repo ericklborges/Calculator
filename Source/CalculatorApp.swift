6812749e344d9fad1877995220183f0652cb090f
Erick Borges <erick.borges@stone.com.br>
2021-11-12 15:30:10 -0300

    chore: create project

---
+++ Source/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by erick.borges on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tests/CalculatorTests.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by erick.borges on 12/11/21.
+//
+
+import XCTest
+
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+
+}
