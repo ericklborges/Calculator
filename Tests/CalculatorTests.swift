8f2f0067b8c57653a22a9219199869dfebfe40a7
Erick Borges <erick.borges@stone.com.br>
2021-11-12 15:55:53 -0300

    feat: add operation tests

--- Tests/CalculatorTests.swift
+++ Tests/CalculatorTests.swift
@@ -18,12 +18,60 @@ class CalculatorTests: XCTestCase {
         sut = Calculator()
     }
 
-    func test_foo() {
-        // Given (dado que)
+    // MARK: - Initial State
 
-        // When (quando)
+    func test_initial_result() {
+        // Given (dado que)
+        sut = Calculator()
 
         // Then (então)
+        XCTAssertEqual(sut.result, 0)
+    }
+
+    // MARK: - Operations
+
+    func test_operation_sum() {
+        // Given (dado que)
+        sut = Calculator()
+
+        // When (quando)
+        sut.sum(10, 5)
+
+        // Then (então)
+        XCTAssertEqual(sut.result, 15)
+    }
+
+    func test_operation_subtract() {
+        // Given (dado que)
+        sut = Calculator()
+
+        // When (quando)
+        sut.subtract(10, 5)
+
+        // Then (então)
+        XCTAssertEqual(sut.result, 5)
+    }
+
+    func test_operation_multiply() {
+        // Given (dado que)
+        sut = Calculator()
+
+        // When (quando)
+        sut.multiply(10, 5)
+
+        // Then (então)
+        XCTAssertEqual(sut.result, 50)
+    }
+
+    func test_operation_divide() {
+        // Given (dado que)
+        sut = Calculator()
+
+        // When (quando)
+        sut.divide(10, 5)
+
+        // Then (então)
+        XCTAssertEqual(sut.result, 2)
     }
 
 }
