dcbcefe93b03c5b219b346bdae3a4addfdf18b6b
Erick Borges <erick.borges@stone.com.br>
2021-11-16 17:36:19 -0300

    add: power challenge

--- Source/Calculator.swift
+++ Source/Calculator.swift
@@ -42,4 +42,24 @@ class Calculator {
             result = lhs / rhs
         }
     }
+
+    // MARK: - Challenge Operations
+
+    func power(_ lhs: Double, _ rhs: Int) {
+        if rhs == 0 {
+            // X^0 = 1
+            result = 1
+
+        } else if lhs == 0 {
+            // 0^-Y = infinito
+            result = .infinity
+
+        } else if rhs < 0 {
+            // X^-Y = (1 / X)^Y
+            result = Array(repeating: 1/lhs, count: abs(rhs)).reduce(1, *)
+
+        } else {
+            result = Array(repeating: lhs, count: rhs).reduce(1, *)
+        }
+    }
 }
